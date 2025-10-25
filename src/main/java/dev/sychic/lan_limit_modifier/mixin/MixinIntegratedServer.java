215c1aced8533c516a1b6456b18668695f562bcb
sychic <47618543+Sychic@users.noreply.github.com>
2025-10-25 16:25:09 -0400

    increase player cap dynamically

--- src/main/java/dev/sychic/lan_limit_modifier/mixin/MixinIntegratedServer.java
+++ src/main/java/dev/sychic/lan_limit_modifier/mixin/MixinIntegratedServer.java
@@ -1,17 +1,30 @@
 package dev.sychic.lan_limit_modifier.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.mojang.datafixers.DataFixer;
 import net.minecraft.client.server.IntegratedServer;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.Services;
+import net.minecraft.server.WorldStem;
+import net.minecraft.server.level.progress.LevelLoadListener;
+import net.minecraft.server.packs.repository.PackRepository;
+import net.minecraft.world.level.storage.LevelStorageSource;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
+import java.net.Proxy;
+
 @Mixin(IntegratedServer.class)
-public class MixinIntegratedServer {
+public abstract class MixinIntegratedServer extends MinecraftServer {
+    public MixinIntegratedServer(Thread thread, LevelStorageSource.LevelStorageAccess levelStorageAccess, PackRepository packRepository, WorldStem worldStem, Proxy proxy, DataFixer dataFixer, Services services, LevelLoadListener levelLoadListener) {
+        super(thread, levelStorageAccess, packRepository, worldStem, proxy, dataFixer, services, levelLoadListener);
+    }
+
     @ModifyReturnValue(method = "getMaxPlayers", at = @At("RETURN"))
     private int modifyMaxPlayers(int original) {
         if (System.getProperty("lanlimit.value") != null) {
             return Integer.parseInt(System.getProperty("lanlimit.value"));
         }
-        return original;
+        return this.getPlayerCount() + 1;
     }
 }
