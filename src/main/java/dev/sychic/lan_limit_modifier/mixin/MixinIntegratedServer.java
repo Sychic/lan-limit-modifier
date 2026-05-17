6d18f7cb3201ea1e9c21951063c285a034b125e4
sychic <47618543+Sychic@users.noreply.github.com>
2026-05-17 16:44:59 -0400

    update to 26.1
    update gradle, and loom

--- src/main/java/dev/sychic/lan_limit_modifier/mixin/MixinIntegratedServer.java
+++ src/main/java/dev/sychic/lan_limit_modifier/mixin/MixinIntegratedServer.java
@@ -8,16 +8,18 @@ import net.minecraft.server.Services;
 import net.minecraft.server.WorldStem;
 import net.minecraft.server.level.progress.LevelLoadListener;
 import net.minecraft.server.packs.repository.PackRepository;
+import net.minecraft.world.level.gamerules.GameRules;
 import net.minecraft.world.level.storage.LevelStorageSource;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 import java.net.Proxy;
+import java.util.Optional;
 
 @Mixin(IntegratedServer.class)
 public abstract class MixinIntegratedServer extends MinecraftServer {
-    public MixinIntegratedServer(Thread thread, LevelStorageSource.LevelStorageAccess levelStorageAccess, PackRepository packRepository, WorldStem worldStem, Proxy proxy, DataFixer dataFixer, Services services, LevelLoadListener levelLoadListener) {
-        super(thread, levelStorageAccess, packRepository, worldStem, proxy, dataFixer, services, levelLoadListener);
+    public MixinIntegratedServer(Thread serverThread, LevelStorageSource.LevelStorageAccess storageSource, PackRepository packRepository, WorldStem worldStem, Optional<GameRules> gameRules, Proxy proxy, DataFixer fixerUpper, Services services, LevelLoadListener levelLoadListener, boolean propagatesCrashes) {
+        super(serverThread, storageSource, packRepository, worldStem, gameRules, proxy, fixerUpper, services, levelLoadListener, propagatesCrashes);
     }
 
     @ModifyReturnValue(method = "getMaxPlayers", at = @At("RETURN"))
